f3dfc3feb0455609fc4e1be7159629d93d4bf8eb
Hakim Cherfi <hakimcherfi31@gmail.com>
2022-03-31 12:27:46 +0000

    Code server_move modifié by amgz

--- src/projet-integration-sri-2021-2022/navigation/script/server_move.py
+++ src/projet-integration-sri-2021-2022/navigation/script/server_move.py
@@ -16,32 +16,43 @@ from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
 from navigation.srv import *
 
 
-pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10) #Liaison avec topic 
 
 def handle_move(req):
+
     rospy.loginfo(req)
     pose = PoseStamped()
     pose.header = std_msgs.msg.Header()
     pose.header.stamp = rospy.Time.now()
     pose.header.frame_id = "map" 
     pose.pose = Pose()
-    pose.pose.position.x = req.x #  kinect Z value, [2], is X in TF of camera_link
-    pose.pose.position.y = req.y # kinect X value, [0], is -Y in TF of camera_link
-    pose.pose.position.z = 0.0 # kinect Y value, [1], is -Z in TF of camera_link
+
+    pose.pose.position.x = req.x 
+    pose.pose.position.y = req.y 
+    pose.pose.position.z = 0.0 
     pose.pose.orientation.x = 0.0
     pose.pose.orientation.y = 0.0
     pose.pose.orientation.z = 0.0
     pose.pose.orientation.w = 1.0
     
-    pub.publish(pose)
+    pub.publish(pose) #Envoie de la pose (x,y,z)
     return 0
 
 
 def add_move_server():
     
-    rospy.init_node('move_base_server')
-    s = rospy.Service('/sri22/move_base',move_base,handle_move)
+    rospy.init_node('move_base_server') #initialisation du node 
+    s = rospy.Service('/sri22/move_base',move_base,handle_move) #Appel du service /sri/move_base
+    if s :  #verification de la connexion
+
+        print("Ready to go !!!!")
+
+    else:
+
+        print("Calling server failed !!!!")   
+
     rospy.spin()
+
 
 if __name__=="__main__":
     add_move_server()
