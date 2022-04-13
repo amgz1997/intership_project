2ee36ec79935680c47f6d4ded717a7efec3b38ac
Hakim Cherfi <hakimcherfi31@gmail.com>
2022-04-13 09:23:36 +0000

    SCript server & client écrit pour navigation ensuite faire bhv tree

--- src/projet-integration-sri-2021-2022/navigation/script/client_move_to_goal.py
+++ src/projet-integration-sri-2021-2022/navigation/script/client_move_to_goal.py
@@ -1,51 +1,99 @@
 #! /usr/bin/env python
-from __future__ import print_function
 import rospy
-
-# Brings in the SimpleActionClient
 import actionlib
+import mbf_msgs.msg as mbf_msgs
+import move_base_msgs.msg as mb_msgs
+from actionlib_msgs.msg import GoalStatus
 
-# Brings in the messages used by the fibonacci action, including the
-# goal message and the result message.
-import actionlib_tutorials.msg
-import navigation.msg
+def create_goal(x, y, z, xx, yy, zz, ww):
 
-def navigation_client():
-    # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
-    client = actionlib.SimpleActionClient('navigation', navigation.msg.NavigationAction)
+    goal = mb_msgs.MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.position.z = z
+    goal.target_pose.pose.orientation.x = xx
+    goal.target_pose.pose.orientation.y = yy
+    goal.target_pose.pose.orientation.z = zz
+    goal.target_pose.pose.orientation.w = ww
+        
+    return goal
 
-    # Waits until the action server has started up and started
-    # listening for goals.
-    client.wait_for_server()
-	
-    goal = navigation.msg.NavigationGoal()
+def move(goal):
 
-    # Creates a goal to send to the action server.
-    goal.target.header.frame_id = "map"
-    goal.target.pose.position.x = 0.0
-    goal.target.pose.position.y = 0.0
-    goal.target.pose.position.z = 0.0
-    goal.target.pose.orientation.x = 0.0
-    goal.target.pose.orientation.y = 0.0
-    goal.target.pose.orientation.z = 0.0
-    goal.target.pose.orientation.w = 1.0
-
-    # Sends the goal to the action server.
-    client.send_goal(goal)
-
-    # Waits for the server to finish performing the action.
+    client.send_goal(goal) #feedback_cb=cb_feeback)
     client.wait_for_result()
+    return client.get_state() == GoalStatus.SUCCEEDED
 
-    # Prints out the result of executing the action
-    return client.get_result()  # A FibonacciResult
+
+def nav():
+
+    goals = [   create_goal(0.0, 0.0, 0, 0, 0, 0.0, 1),
+                create_goal(0.1, 0.1, 0, 0, 0, 0.0, 1),
+                
+    ]
+
+    for goal in goals:
+        rospy.loginfo("Attempting to drive to %s %s", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
+        if not move(goal):
+            return False
+
+    return True
 
 if __name__ == '__main__':
+
     try:
-        # Initializes a rospy node so that the SimpleActionClient can
-        # publish and subscribe over ROS.
-        rospy.init_node('navigation_client_py')
-        result = navigation_client()
-        print("Result: ", result.result_code)
+
+        rospy.init_node('client_navigation')
+        client = actionlib.SimpleActionClient('move_base', mb_msgs.MoveBaseAction)
+        client.wait_for_server()
+        rospy.loginfo("Connected to SimpleActionServer 'move_base'")
+        result = nav()
+        rospy.loginfo("Drove to waypoints: %s", result)
+        #print("Result:", ', '.join([str(n) for n in result.sequence]))
+
     except rospy.ROSInterruptException:
-        print("program interrupted before completion", file=sys.stderr)
+        rospy.logerror("program interrupted before completion")
+
+
+# from __future__ import print_function
+# import rospy
+
+    
+#  # Brings in the SimpleActionClient
+# import actionlib
+# # Brings in the messages used by the fibonacci action, including the
+#  # goal message and the result message.
+# import actionlib_tutorials.msg
+ 
+# def fibonacci_client():
+#     # Creates the SimpleActionClient, passing the type of the action
+#     # (FibonacciAction) to the constructor.
+#     client = actionlib.SimpleActionClient('fibonacci', actionlib_tutorials.msg.FibonacciAction)
+
+#     # Waits until the action server has started up and started
+#     # listening for goals.
+#     client.wait_for_server()
+
+#     # Creates a goal to send to the action server.
+#     goal = actionlib_tutorials.msg.FibonacciGoal(order=20)
+
+#     # Sends the goal to the action server.
+#     client.send_goal(goal)
+
+#     # Waits for the server to finish performing the action.
+#     client.wait_for_result()
+
+#     # Prints out the result of executing the action
+#     return client.get_result()  # A FibonacciResult
+
+# if __name__ == '__main__':
+#     try:
+#                 # Initializes a rospy node so that the SimpleActionClient can
+#         # publish and subscribe over ROS.
+#         rospy.init_node('fibonacci_client_py')
+#         result = fibonacci_client()
+#         print("Result:", ', '.join([str(n) for n in result.sequence]))
+#     except rospy.ROSInterruptException:
+#         print("program interrupted before completion", file=sys.stderr)
--- src/projet-integration-sri-2021-2022/navigation/script/server_move.py
+++ src/projet-integration-sri-2021-2022/navigation/script/server_move.py
@@ -1,70 +1,90 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import rospy
-from geometry_msgs.msg import Twist
-import sys
-import time
-import os
-import numpy as np
+
+# import actionlib
+# import rospy
+# import mbf_msgs.msg as mbf_msgs
+# import move_base_msgs.msg as mb_msgs
+
+# def mb_execute_cb(msg):
+#     mbf_mb_ac.send_goal(mbf_msgs.MoveBaseGoal(target_pose=msg.target_pose),
+#                         feedback_cb=mbf_feedback_cb)
+
+#     rospy.logdebug("Relaying move_base goal to mbf")
+#     mbf_mb_ac.wait_for_result()
+
+#     status = mbf_mb_ac.get_state()
+#     result = mbf_mb_ac.get_result()
+
+#     rospy.logdebug("MBF execution completed with result [%d]: %s", result.outcome, result.message)
+#     if result.outcome == mbf_msgs.MoveBaseResult.SUCCESS:
+#         mb_as.set_succeeded(mb_msgs.MoveBaseResult(), "Goal reached.")
+#     else:
+#         mb_as.set_aborted(mb_msgs.MoveBaseResult(), result.message)
+
+# def mbf_feedback_cb(feedback):
+#     mb_as.publish_feedback(mb_msgs.MoveBaseFeedback(base_position=feedback.current_pose))
+
+# if __name__ == '__main__':
+#     rospy.init_node("move_base1")
+
+#     # move_base_flex get_path and move_base action clients
+#     mbf_mb_ac = actionlib.SimpleActionClient("move_base_flex/move_base", mbf_msgs.MoveBaseAction)
+#     mbf_mb_ac.wait_for_server(rospy.Duration(10))
+
+#     mb_as = actionlib.SimpleActionServer('move_base', mb_msgs.MoveBaseAction, mb_execute_cb, auto_start=False)
+#     mb_as.start()
+#     rospy.spin()
+
+import rospy 
 import actionlib
-import actionlib_tutorials.msg
-import navigation.msg
-import std_msgs
+import actionlib_tutorials.msg 
+class FibonacciAction(object):
+    # create messages that are used to publish feedback/result
+    _feedback = actionlib_tutorials.msg.FibonacciFeedback()
+    _result = actionlib_tutorials.msg.FibonacciResult()
 
-#from tf.transformations import quaternion_from_euler
-
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
-from navigation.srv import *
-
-
-
-pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10) #Liaison avec topic 
-
-def handle_move(req):
-
-    rospy.loginfo(req)
-    pose = PoseStamped()
-    pose.header = std_msgs.msg.Header()
-    pose.header.stamp = rospy.Time.now()
-    pose.header.frame_id = "map" 
-    pose.pose = Pose()
-
-    pose.pose.position.x = req.x 
-    pose.pose.position.y = req.y 
-    pose.pose.position.z = 0.0 
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
     
-    pose.pose.orientation.x = 0.0  
-    pose.pose.orientation.y = 0.0 
-    pose.pose.orientation.z = req.theta  # Orientation theta 
-    pose.pose.orientation.w = 1.0
+    def execute_cb(self, goal):
+        # helper variables
+        r = rospy.Rate(1)
+        success = True
+        
+        # append the seeds for the fibonacci sequence
+        self._feedback.sequence = []
+        self._feedback.sequence.append(0)
+        self._feedback.sequence.append(1)
+        
+        # publish info to the console for the user
+        rospy.loginfo('%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
+        
+        # start executing the action
+        for i in range(1, goal.order):
+            # check that preempt has not been requested by the client
+            if self._as.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self._action_name)
+                self._as.set_preempted()
+                success = False
+                break
+            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
+            # publish the feedback
+            self._as.publish_feedback(self._feedback)
+            # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+            r.sleep()
 
-   # pose.pose.orientation.x = 0.0  #req.x*np.pi/180
-   # pose.pose.orientation.y = 0.0 #req.y*np.pi/180
-   # pose.pose.orientation.z = 0.0
-   # pose.pose.orientation.w = 1.0
-   # q=tf.Quaternions(0,0,theta) 
-
-    
-    pub.publish(pose) #Envoie de la pose (x,y,theta)
-    return 0
-
-
-def add_move_server():
-    
-    rospy.init_node('move_base_server') #initialisation du node 
-    s = rospy.Service('/sri22/move_base',move_base,handle_move) #Appel du service /sri/move_base
-    if s :  #verification de la connexion
-
-        print("Ready to go !!!!")
-
-    else:
-
-        print("Calling server failed !!!!")   
-
+        if success:
+            self._result.sequence = self._feedback.sequence
+            rospy.loginfo('%s: Succeeded' % self._action_name)
+            self._as.set_succeeded(self._result)
+        
+if __name__ == '__main__':
+    rospy.init_node('fibonacci')
+    server = FibonacciAction(rospy.get_name())
     rospy.spin()
 
 
-if __name__=="__main__":
 
-    add_move_server()
