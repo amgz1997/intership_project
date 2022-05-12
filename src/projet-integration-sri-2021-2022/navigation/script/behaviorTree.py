d8311a9a2d53ad182255324f149caf3de4a41d18
Hakim Cherfi <hakimcherfi31@gmail.com>
2022-05-12 14:55:14 +0000

    torso and arm by amgz

--- src/projet-integration-sri-2021-2022/navigation/script/behaviorTree.py
+++ src/projet-integration-sri-2021-2022/navigation/script/behaviorTree.py
@@ -19,11 +19,15 @@ from geometry_msgs.msg import PoseStamped
 from aruco_msgs.msg import Marker
 from aruco_msgs.msg import MarkerArray
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
-#from moveit_msgs.msg import MoveGroupGoal, MoveGroupResult, MoveGroupAction, Constraints,MoveItErrorCodes, JointConstraint
+from moveit_msgs.msg import MoveGroupGoal, MoveGroupResult, MoveGroupAction, Constraints, MoveItErrorCodes, JointConstraint, MoveGroupActionGoal, MotionPlanRequest
 from std_msgs.msg import Header
 import moveit_commander
 import moveit_msgs.msg  
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal 
+from trajectory_msgs.msg import JointTrajectoryPoint, JointTrajectory 
 
+#from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryFeeback, FollowJointTrajectoryResult
+#from trajectory_msgs.msg import JointTrajectoryPoint
 #from reactive_sequence import RSequence
 
 class movebase(pt.behaviour.Behaviour):
@@ -50,12 +54,12 @@ class movebase(pt.behaviour.Behaviour):
 
       if self.pose=="point1":
 
-        goals=self.create_goal(-0.9, 10.075, 1.0)
+        goals=self.create_goal(2.33, 0.588, 1.0)
         self.client.send_goal(goals)
 
       elif self.pose=="point2":
 
-        goals=self.create_goal(2.642, 9.96, 1.0)
+        goals=self.create_goal(0.0, 0.0, 1.0)
         self.client.send_goal(goals)
 
       #self.at_position=True
@@ -153,52 +157,139 @@ class arm_arng(pt.behaviour.Behaviour):
 
         
         rospy.loginfo("Arm tucking")
+        self.arm_cmd = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=1)
+        self.torso_cmd = rospy.Publisher('/torso_controller/command', JointTrajectory, queue_size=1)
 
-        self.config=config
-        moveit_commander.roscpp_initialize(sys.argv)
-        #rospy.init_node('move_group_python_interface_tutorial',anonymous=True)
+        # self.config=config
+
+        # moveit_commander.roscpp_initialize(sys.argv)
+        # self.robot=moveit_commander.RobotCommander()
+        # self.scene=moveit_commander.PlanningSceneInterface()
+        # self.group1=moveit_commander.MoveGroupCommander("arm_torso")         
+        # self.display_trajectory_publisher=rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory,queue_size=1) 
+        
+        # self.moveit_ac=SimpleActionClient('/move_group', MoveGroupAction)
+        # self.moveit_ac.wait_for_server()
+
+        rospy.loginfo("connected to /arm_controller")
+
+        # if not self.motion_ac.wait_for_server(rospy.Duration(5.0)):
+        #     rospy.logerr("Could not connect to /tts action server!")
+        #     exit(0)
+        rospy.loginfo("Connected!")
 
         self.sent_goal=False
-
         
         super(arm_arng, self).__init__("Tucking_Arm")
 
     def update(self):
-
-    #   elif self.config=="config2":
-    #     joint_goal=self.group1.get_current_joint_values()
-    #     joint_goal=[0.34, 0.05, -0.07, -1.0, 1.5 ,-1.57, 1.52, 0.0]
-    #     self.group1.go(joint_goal, wait=True)
-      joint_goal=self.moveit_create_goal(0.15, 0.20, -1.34, -0.2, 1.94 ,-1.57, 1.37, 0.0)
-
-      if self.sent_goal:
-          return pt.common.Status.SUCCESS
-
-      else :
-          return pt.common.Status.FAILURE
-
-    def moveit_create_goal(self,torso,arm1,arm2,arm3,arm4,arm5,arm6,arm7):
-
-        self.robot=moveit_commander.RobotCommander()
-        self.scene=moveit_commander.PlanningSceneInterface()
-        self.group1=moveit_commander.MoveGroupCommander("arm_torso")
-        self.display_trajectory_publisher=rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory)  
-       # self.display_trajectory_publisher=rospy.Publisher('/move_group/goal',MoveBaseActionGoal)  
-
-        joint_goal=self.group1.get_current_joint_values()
-      #print("%s" % joint_goal)
-      
-      #if self.config=="config1":
-
-        joint_goal=[torso, arm1, arm2, arm3, arm4,arm5, arm6, arm7]
-
-        #Essayer 
-        #joint_goal=self.group1.set_current_joint_values(joint_goal)
-        #self.plan1=group1.plan()
-
-        self.group1.go(joint_goal, wait=True) 
+   ##################################################     
        
+        rospy.sleep(1)
+
+####################################################
+
+        # rospy.sleep(1.0)
+
+        # joint_goal=self.moveit_create_goal(0.25, 0.20, -1.34, -0.2, 1.94 ,-1.57, 1.37, 0.0)
+
+        # elif self.config=="config2":
+        # joint_goal=self.group1.get_current_joint_values()
+
+        # joint_posi=[0.30, 0.20, -1.34, -0.2, 1.94 ,-1.57, 1.37, 0.0]
+        # joint_name=["torso_lift_joint", "arm_1_joint", "arm_2_joint", "arm_3_joint", "arm_4_joint", "arm_5_joint", "arm_6_joint", "arm_7_joint"]
+
+        #  self.group1.go(joint_goal, wait=True)
+
+        # print("A")
+
+        # joint_goal=self.moveit_create_goal(joint_name, joint_posi, group="arm_torso", plan_only=True)
+        # self.moveit_ac.send_goal(joint_goal)
+
+############################################        
+    #     g=PlayMotionGoal()
+    #     g.motion_name='pregrasp'
+    #     g.skip_planning=False
+    #    # motion=self.play_motion_goal()
+    #     self.motion_ac.send_goal_and_wait(g)
+##################################################
+
+        jt=JointTrajectory()
+        jt.joint_names=['arm_1_joint', 'arm_2_joint', 'arm_3_joint', 'arm_4_joint', 'arm_5_joint', 'arm_6_joint', 'arm_7_joint']
+        jtp=JointTrajectoryPoint()
+        jtp.positions =[0.2, -1.2, -0.2, 1.94 ,-1.57, 1.37, 0.0]
+        jtp.time_from_start = rospy.Duration(3.0)
+        jt.points.append(jtp)
+        self.arm_cmd.publish(jt)
+
+        rospy.sleep(5)
+
+        tor=JointTrajectory()
+        tor.joint_names=['torso_lift_joint']
+        tor_p=JointTrajectoryPoint()
+        tor_p.positions =[0.25]
+        tor_p.time_from_start = rospy.Duration(2.5)
+        tor.points.append(tor_p)
+        self.torso_cmd.publish(tor)
+
+        if self.sent_goal:
+
+            return pt.common.Status.FAILURE
+
+        else:
+
+            return pt.common.Status.SUCCESS
+
+   # def moveit_create_goal(self,torso,arm1,arm2,arm3,arm4,arm5,arm6,arm7):
+   # def moveit_create_goal(self,joint_names, joint_values, group="arm_torso", plan_only=True):
+   # def play_motion_goal(self):
+
+  #################################################################################
+
+        # joint_goal=self.group1.get_current_joint_values()
+        # joint_goal=[torso, arm1, arm2, arm3, arm4,arm5, arm6, arm7]
+        # #joint_goal=[0.15, 0.20, -1.34, -0.2, 1.94 ,-1.57, 1.37, 0.0]
+        # self.group1.set_joint_value_target(joint_goal)
+        # plan1=self.group1.plan()
+        # self.group1.set_planning_time(5.0)
+        # return self.group1.go(wait=True)
+###################################################################################
+
+        #self.group1.stop() 
+        # header=Header()
+        # header.frame_id='base_footprint'
+        # header.stamp=rospy.Time.now()
+        # moveit_goal=MoveGroupGoal()
+        # goal_c=Constraints()
+        # goal_c.header.header.frame_id='base_footprint'
+        # for name, value in zip(joint_names, joint_values):
+        #     joint_c= JointConstraint()
+        #     joint_c.joint_name= name
+        #     joint_c.position= value
+        #     joint_c.tolerance_above=0.01
+        #     joint_c.tolerance_below=0.01
+        #     joint_c.weight=1.0        
+        #     goal_c.joint_constraints.append(joint_c)   
+        # moveit_goal.request.goal_constraints.append(goal_c)
+        # moveit_goal.request.max_velocity_scaling_factor=0.25
+        # moveit_goal.request.planner_id="arm_torso"
+        # moveit_goal.request.num_planning_attempts=1
+        # moveit_goal.request.allowed_planning_time=15.0
+        # moveit_goal.planning_options.plan_only=plan_only
+        # moveit_goal.planning_options.planning_scene_diff.is_diff= True 
+        # moveit_goal.request.group_name= group        
+        # return  self.moveit_ac.send_goal(moveit_goal) #self.moveit_ac.send_goal(moveit_goal.goal)
+        # #return self.moveit_goal.goal
+        #g=PlayMotionGoal()
+        #g.motion_name='pregrasp'
+        #return g
+#######################################################################################
+
+
+        #Essayer de faire une seul ou deux fonctions pour torso & arm
+
 # Faire pick + octomap (voir lien )
+
 class BehaviourTree(ptr.trees.BehaviourTree):
 
     def __init__(self):
@@ -209,20 +300,20 @@ class BehaviourTree(ptr.trees.BehaviourTree):
         nav2=movebase("point2")
         detect=aruco_detect()
         arm_tucking1=arm_arng("config1")
-        
-        #arm_tucking2=arm_arng("config2")
 
+        #arm_tucking2=arm_arng("config2")
         root=pt.composites.Sequence(name="Task")
-        root.add_children([nav1,arm_tucking1,detect,nav2])
+        root.add_children([arm_tucking1])
 
         #root.add_children([arm_tucking1])
+
         #print(pt.display.ascii_tree(root))
 
         super(BehaviourTree, self).__init__(root)
         rospy.sleep(5)
         self.setup(timeout=15)
 
-        while not rospy.is_shutdown(): self.tick_tock(10000)
+        while not rospy.is_shutdown(): self.tick_tock(500)
 
 if __name__ == '__main__':
 
@@ -235,5 +326,5 @@ if __name__ == '__main__':
         except rospy.ROSInterruptException:
              pass
 
-        rospy.spin()          
+       # rospy.spin()         
           
--- src/projet-integration-sri-2021-2022/navigation/script/server_move.py
+++ src/projet-integration-sri-2021-2022/navigation/script/server_move.py
@@ -1,139 +1,160 @@
 #!/usr/bin/env python
 
 
-# import actionlib
-# import rospy
-# import mbf_msgs.msg as mbf_msgs
-# import move_base_msgs.msg as mb_msgs
+# import argparse
+# import geometry_msgs.msg as geometry_msgs
+# import move_base_msgs.msg as move_base_msgs
+# from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+# import py_trees_ros
+# #from . import action_server
+# import rclpy
+# import sys
 
-# def mb_execute_cb(msg):
-#     mbf_mb_ac.send_goal(mbf_msgs.MoveBaseGoal(target_pose=msg.target_pose),
-#                         feedback_cb=mbf_feedback_cb)
-
-#     rospy.logdebug("Relaying move_base goal to mbf")
-#     mbf_mb_ac.wait_for_result()
-
-#     status = mbf_mb_ac.get_state()
-#     result = mbf_mb_ac.get_result()
-
-#     rospy.logdebug("MBF execution completed with result [%d]: %s", result.outcome, result.message)
-#     if result.outcome == mbf_msgs.MoveBaseResult.SUCCESS:
-#         mb_as.set_succeeded(mb_msgs.MoveBaseResult(), "Goal reached.")
-#     else:
-#         mb_as.set_aborted(mb_msgs.MoveBaseResult(), result.message)
-
-# def mbf_feedback_cb(feedback):
-#     mb_as.publish_feedback(mb_msgs.MoveBaseFeedback(base_position=feedback.current_pose))
-
-# if __name__ == '__main__':
-#     rospy.init_node("move_base1")
-
-#     # move_base_flex get_path and move_base action clients
-#     mbf_mb_ac = actionlib.SimpleActionClient("move_base_flex/move_base", mbf_msgs.MoveBaseAction)
-#     mbf_mb_ac.wait_for_server(rospy.Duration(10))
-
-#     mb_as = actionlib.SimpleActionServer('move_base', mb_msgs.MoveBaseAction, mb_execute_cb, auto_start=False)
-#     mb_as.start()
-#     rospy.spin()
-
-
-##########################################
-
-
-import argparse
-import geometry_msgs.msg as geometry_msgs
-import move_base_msgs.msg as move_base_msgs
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-import py_trees_ros
-#from . import action_server
-import rclpy
-import sys
-
-##############################################################################
-# Class
-##############################################################################
+# ##############################################################################
+# # Class
+# ##############################################################################
 
 
 
-class MoveBase(py_trees.behaviour.Behaviour):
-    """
-    Simulates a move base style interface.
+# class MoveBase(py_trees.behaviour.Behaviour):
+#     """
+#     Simulates a move base style interface.
 
-    Node Name:
-        * **move_base_controller**
+#     Node Name:
+#         * **move_base_controller**
 
-    Action Servers:
-        * **/move_base** (:class:`py_trees_ros_interfaces.action.MoveBase`)
+#     Action Servers:
+#         * **/move_base** (:class:`py_trees_ros_interfaces.action.MoveBase`)
 
-          * point to point move base action
+#           * point to point move base action
 
-    Args:
-        duration: mocked duration of a successful action
-    """
-    def __init__(self, duration=None):
-        super().__init__(
-            node_name="move_base_controller",
-            action_name="move_base",
-            action_type=move_base_msgs.MoveBase,
-            generate_feedback_message=self.generate_feedback_message,
-            duration=duration
-        )
-        self.pose = MoveBaseGoal()
-        self.goal.target_pose.header.frame_id = "map"
-        #self.pose.pose.position = geometry_msgs.Point(x=0.0, y=0.0, z=0.0)
-        self.goal.target_pose.header.stamp = rospy.Time.now()
-        self.goal.target_pose.pose.position.x = 2.3
-        self.goal.target_pose.pose.position.y = 0.588
-        self.goal.target_pose.pose.position.z = 0.0
+#     Args:
+#         duration: mocked duration of a successful action
+#     """
+#     def __init__(self, duration=None):
+#         super().__init__(
+#             node_name="move_base_controller",
+#             action_name="move_base",
+#             action_type=move_base_msgs.MoveBase,
+#             generate_feedback_message=self.generate_feedback_message,
+#             duration=duration
+#         )
+#         self.pose = MoveBaseGoal()
+#         self.goal.target_pose.header.frame_id = "map"
+#         #self.pose.pose.position = geometry_msgs.Point(x=0.0, y=0.0, z=0.0)
+#         self.goal.target_pose.header.stamp = rospy.Time.now()
+#         self.goal.target_pose.pose.position.x = 2.3
+#         self.goal.target_pose.pose.position.y = 0.588
+#         self.goal.target_pose.pose.position.z = 0.0
 
-        #quat = tf.transformations.quaternion_from_euler(0, 0, theta)
-        self.goal.target_pose.pose.orientation.x = 0.0
-        self.goal.target_pose.pose.orientation.y = 0.0
-        self.goal.target_pose.pose.orientation.z = 0.0
-        self.goal.target_pose.pose.orientation.w = 1.0
-        return goal
+#         #quat = tf.transformations.quaternion_from_euler(0, 0, theta)
+#         self.goal.target_pose.pose.orientation.x = 0.0
+#         self.goal.target_pose.pose.orientation.y = 0.0
+#         self.goal.target_pose.pose.orientation.z = 0.0
+#         self.goal.target_pose.pose.orientation.w = 1.0
+#         return goal
 
-    def generate_feedback_message(self) :# -> py_trees_actions.MoveBase.Feedback:
-        """
-        Do a fake pose incremenet and populate the feedback message.
+#     def generate_feedback_message(self) :# -> py_trees_actions.MoveBase.Feedback:
+#         """
+#         Do a fake pose incremenet and populate the feedback message.
 
-        Returns:
-            :class:`py_trees_actions.MoveBase.Feedback`: the populated feedback message
-        """
-        # actually doesn't go to the goal right now...
-        # but we could take the feedback from the action
-        # and increment this to that proportion
-        # self.odometry.pose.pose.position.x += 0.01
-        self.pose.pose.position.x += 0.0
-        msg = py_trees_actions.MoveBase.Feedback()  # .Feedback() is more proper, but indexing can't find it
-        msg.base_position = self.pose
-        return msg
+#         Returns:
+#             :class:`py_trees_actions.MoveBase.Feedback`: the populated feedback message
+#         """
+#         # actually doesn't go to the goal right now...
+#         # but we could take the feedback from the action
+#         # and increment this to that proportion
+#         # self.odometry.pose.pose.position.x += 0.01
+#         self.pose.pose.position.x += 0.0
+#         msg = py_trees_actions.MoveBase.Feedback()  # .Feedback() is more proper, but indexing can't find it
+#         msg.base_position = self.pose
+#         return msg
 
 
 
 
-def main():
-    """
-    Entry point for the mock move base node.
-    """
-    parser = argparse.ArgumentParser(description='Mock a docking controller')
-    command_line_args = rclpy.utilities.remove_ros_args(args=sys.argv)[1:]
-    parser.parse_args(command_line_args)
+# def main():
+#     """
+#     Entry point for the mock move base node.
+#     """
+#     parser = argparse.ArgumentParser(description='Mock a docking controller')
+#     command_line_args = rclpy.utilities.remove_ros_args(args=sys.argv)[1:]
+#     parser.parse_args(command_line_args)
 
-    rclpy.init()  # picks up sys.argv automagically internally
-    move_base = MoveBase()
-    executor = rclpy.executors.MultiThreadedExecutor(num_threads=4)
-    executor.add_node(move_base.node)
+#     rclpy.init()  # picks up sys.argv automagically internally
+#     move_base = MoveBase()
+#     executor = rclpy.executors.MultiThreadedExecutor(num_threads=4)
+#     executor.add_node(move_base.node)
 
+#     try:
+#         executor.spin()
+#     except KeyboardInterrupt:
+#         move_base.abort()
+#         # caveat: often broken, with multiple spin_once or shutdown, error is the
+#         # mysterious:
+#         #   The following exception was never retrieved: PyCapsule_GetPointer
+#         #   called with invalid PyCapsule object
+#         executor.shutdown()  # finishes all remaining work and exits
+
+#     move_base.shutdown()
+#     rclpy.shutdown()
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist 
+from nav_msgs.msg import Odometry
+from moveit_msgs.msg import MoveGroupAction, MoveGroupActionGoal
+from moveit_msgs.msg import Constraints, JointConstraint, PositionConstraint, OrientationConstraint, BoundingVolume
+from naoqi_bridge_msgs.msg import JointAnglesWithSpeed
+
+MoveGroupActionGoal_msg = MoveGroupActionGoal()
+JointAnglesWithSpeed_msg = JointAnglesWithSpeed()
+
+
+
+def dataMoveGoal(data): #MoveGroupActionGoal_msg.goal.request.goal_constraints
     try:
-        executor.spin()
-    except KeyboardInterrupt:
-        move_base.abort()
-        # caveat: often broken, with multiple spin_once or shutdown, error is the
-        # mysterious:
-        #   The following exception was never retrieved: PyCapsule_GetPointer
-        #   called with invalid PyCapsule object
-        executor.shutdown()  # finishes all remaining work and exits
+        #JointAnglesWithSpeed_msg.joint_names = data.goal.request.goal_constraints
+        #JointAnglesWithSpeed_msg.joint_angles = data.goal.request.goal_constraints
+        JointAnglesWithSpeed_msg.joint_names = ['torso_lift_joint', 'arm_1_joint', 'arm_2_joint', 'arm_3_joint', 'arm_4_joint', 'arm_5_joint', 'arm_6_joint', 'arm_7_joint']
+        JointAnglesWithSpeed_msg.joint_angles = [0.15, 0.05, -0.07, -1.0, 1.5 ,-1.57, 1.52, 0.0]
+        JointAnglesWithSpeed_msg.speed = 0.25
+        JointAnglesWithSpeed_msg.relative = 0
+    
+        # sad se tu treba igrati sa stringovima. napraviti petljicu
+        for i in data.goal.request.goal_constraints:
+            rospy.loginfo("%s ",i)
+           
+        rospy.loginfo("%s ",data.goal.request.goal_constraints)
+    except:
+        rospy.loginfo("error happens in data MoveGoal")
+        rospy.loginfo("%s ",data.goal.request.goal_constraints)
+        for i in data.goal.request.goal_constraints:
+            rospy.loginfo("%s ",i)
+           
 
-    move_base.shutdown()
-    rclpy.shutdown()
+def move_group():
+    joint_angles_pub = rospy.Publisher('/tiago_robot/pose/joint_angles', JointAnglesWithSpeed, queue_size=10)
+    moveit_group_goal_sub = rospy.Subscriber('/move_group/goal', MoveGroupActionGoal, dataMoveGoal) 
+    rospy.init_node('MoveGroup', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+  
+    rospy.loginfo("move group")
+
+    while(1):
+        JointAnglesWithSpeed_msg.header.frame_id = "torso" # "base_footprint"
+        JointAnglesWithSpeed_msg.header.stamp = rospy.Time.now()
+    
+        joint_angles_pub.publish(JointAnglesWithSpeed_msg)
+
+
+        #rospy.loginfo("%s ",JointAnglesWithSpeed_msg)
+        rospy.sleep(2)
+    rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        move_group()
+    except rospy.ROSInterruptException:
+        pass
